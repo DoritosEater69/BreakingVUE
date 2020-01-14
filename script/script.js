324f0da24b82db1363dd0a4dec23caac869a61de
Niclas Koch <n.koch@medicalvision.de>
2020-01-14 17:34:31 +0100

    INIT - Working Version

---
+++ script/script.js
@@ -0,0 +1,40 @@
+new Vue({
+
+	el: '#app',
+	data: {
+		import: [],
+		filter: '',
+		titleText: 'from Breaking bad',
+		filters: ['characters', 'episodes', 'quotes', 'deaths'],
+	},
+
+	methods: {
+		//Update API
+		setFilter: function (filter) {
+			this.filter = filter.toLowerCase();
+			axios
+				.get('https://www.breakingbadapi.com/api/' + filter)
+				.then(response => (this.import = response.data))
+		},
+
+		orderNameAsc: function () {
+			var arr = [];
+			console.log("in Function");
+			let imported = this.import;
+
+			return imported
+				.sort(function (a, b) {
+					return a.author.localeCompare(b.author);
+				});
+		},
+
+		//Get API once
+		created() {
+			axios
+				.get('https://www.breakingbadapi.com/api/quotes')
+				.then(response => (this.import = response.data))
+		},
+
+
+	}
+});
